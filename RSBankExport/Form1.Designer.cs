86b27c15f366f586b2c6ca2e68359ca09c816d84
Dmitry Golgovsky <d.westcoast@aol.com>
2017-11-21 09:17:44 +0300

    init commit

---
+++ RSBankExport/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace RSBankExport
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label_DateExport = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label_Sum = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Открыть";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(278, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(304, 201);
+            this.label2.TabIndex = 2;
+            this.label2.Text = resources.GetString("label2.Text");
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Дата выгрузки:";
+            // 
+            // label_DateExport
+            // 
+            this.label_DateExport.AutoSize = true;
+            this.label_DateExport.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_DateExport.Location = new System.Drawing.Point(185, 58);
+            this.label_DateExport.Name = "label_DateExport";
+            this.label_DateExport.Size = new System.Drawing.Size(0, 16);
+            this.label_DateExport.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(12, 95);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(165, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Номер документа:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(185, 95);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(57, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(12, 135);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(127, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Общая сумма:";
+            // 
+            // label_Sum
+            // 
+            this.label_Sum.AutoSize = true;
+            this.label_Sum.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_Sum.Location = new System.Drawing.Point(185, 138);
+            this.label_Sum.Name = "label_Sum";
+            this.label_Sum.Size = new System.Drawing.Size(0, 16);
+            this.label_Sum.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(12, 175);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(160, 35);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Сохранить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label6.Location = new System.Drawing.Point(12, 215);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(570, 100);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "label6";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(594, 328);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label_Sum);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label_DateExport);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Мастер экспорта в Русский Южный Банк";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label_DateExport;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label_Sum;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label6;
+
+    }
+}
+
---
+++ RSBankExport/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace RSBankExport
+{
+    public partial class Form1 : Form
+    {
+        static int EMPLOYERS_COUNT = 0;
+        static int CUR_EXPORT_NUMBER = 0;
+        static String fullPath = Application.StartupPath.ToString();
+        static employer[] EMPLOYERS_LIST = new employer[EMPLOYERS_COUNT];
+
+        public Form1()
+        {
+            InitializeComponent();
+            onLoad();
+            label6.Text = "Прочитано " + EMPLOYERS_COUNT + " сотрудник{ов/а}, имеющих карты РусЮгБанка" + "\n";
+            textBox1.Text = Convert.ToString(CUR_EXPORT_NUMBER);
+        }
+
+        public static void onLoad()
+        {
+            System.IO.DirectoryInfo dirSource = new System.IO.DirectoryInfo(fullPath + "\\Export");
+            if (!dirSource.Exists)
+            {
+                System.IO.Directory.CreateDirectory(fullPath + "\\Export");
+            }
+            System.IO.FileStream fCh = new System.IO.FileStream(fullPath + "\\Export\\12253_00.txt", System.IO.FileMode.OpenOrCreate, System.IO.FileAccess.Read);
+            System.IO.StreamReader SR = new System.IO.StreamReader(fCh, System.Text.Encoding.Default);
+
+            string str = "";
+            while (!SR.EndOfStream)
+            {
+                str = SR.ReadLine();
+                if (str == "")
+                    continue;
+                EMPLOYERS_COUNT++;
+            }
+            SR.Close();
+            fCh.Close();
+
+            Array.Resize<employer>(ref EMPLOYERS_LIST, EMPLOYERS_COUNT);
+            
+            fCh = new System.IO.FileStream(fullPath + "\\Export\\12253_00.txt", System.IO.FileMode.OpenOrCreate, System.IO.FileAccess.Read);
+            SR = new System.IO.StreamReader(fCh, System.Text.Encoding.Default);
+            int i = 0;
+            while (!SR.EndOfStream)
+            {
+                str = SR.ReadLine();
+                if (str == "")
+                    continue;
+                String[] words = str.Split(new char[] { '^' }, StringSplitOptions.RemoveEmptyEntries);
+                EMPLOYERS_LIST[i] = new employer(words[2], words[3], words[4], 0.0);
+                EMPLOYERS_LIST[i].account = words[0];
+                EMPLOYERS_LIST[i].bik = words[1];
+                EMPLOYERS_LIST[i].series = words[5];
+                EMPLOYERS_LIST[i].code = words[6];
+                i++;
+            }
+            SR.Close();
+            fCh.Close();
+            fCh = new System.IO.FileStream(fullPath + "\\RSBank.dat", System.IO.FileMode.OpenOrCreate, System.IO.FileAccess.ReadWrite);
+            System.IO.StreamReader cur_num_stream = new System.IO.StreamReader(fCh);
+            CUR_EXPORT_NUMBER = Convert.ToInt32(cur_num_stream.ReadLine()) + 1;
+            cur_num_stream.Close();
+            fCh.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int loaded = 0;
+            double sum = 0.0;
+            System.Windows.Forms.OpenFileDialog dlg = new System.Windows.Forms.OpenFileDialog();
+            dlg.Filter = "Файлы XML (*.xml)|*.xml";
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                XDocument xdoc = null;
+                try
+                {
+                    xdoc = XDocument.Load(dlg.FileName);
+
+                    int i = 0;
+                    label_DateExport.Text = xdoc.Root.Attribute("ДатаФормирования").Value;
+                    foreach (XElement xe in xdoc.Element("СчетаПК").Elements("ЗачислениеЗарплаты").Elements("Сотрудник"))
+                    {
+                        for (int j = 0; j < EMPLOYERS_COUNT; j++) // j = i, заменено ввиду представления списка не по алфавиту
+                        {
+                            if (xe.Element("Фамилия").Value.ToUpper() == EMPLOYERS_LIST[j].lName &&
+                                xe.Element("Имя").Value.ToUpper() == EMPLOYERS_LIST[j].fName &&
+                                xe.Element("Отчество").Value.ToUpper() == EMPLOYERS_LIST[j].mName)
+                            {
+                                EMPLOYERS_LIST[j].money = double.Parse(xe.Element("Сумма").Value, System.Globalization.CultureInfo.InvariantCulture);
+                                sum += EMPLOYERS_LIST[j].money;
+                                i = j;
+                                loaded++;
+                                break;
+                            }
+                        }
+                    }
+                    label6.Text += "Обработано " + loaded + " сотрудник{ов/а} из файла 1С" + "\n";
+                    label_Sum.Text = sum.ToString("0.00", System.Globalization.CultureInfo.GetCultureInfo("en-US"));
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка: " + ex.Message);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CUR_EXPORT_NUMBER = Convert.ToInt32(textBox1.Text);
+            string fileName = EMPLOYERS_LIST[0].code + "_";
+            if (CUR_EXPORT_NUMBER < 10)
+                fileName += "0";
+            fileName += CUR_EXPORT_NUMBER + ".txt";
+
+            FileStream fs = new FileStream(fullPath + "\\Export\\" + fileName, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamWriter SW = new StreamWriter(fs);
+            for (int i = 0; i < EMPLOYERS_COUNT; i++)
+            {
+                if (EMPLOYERS_LIST[i].money != 0)
+                {
+                    string line = EMPLOYERS_LIST[i].account + "^" +
+                                 EMPLOYERS_LIST[i].bik + "^" +
+                                 EMPLOYERS_LIST[i].money.ToString("0.00", System.Globalization.CultureInfo.GetCultureInfo("en-US")) + "^" +
+                                 EMPLOYERS_LIST[i].lName + "^" +
+                                 EMPLOYERS_LIST[i].fName + "^" +
+                                 EMPLOYERS_LIST[i].mName + "^" +
+                                 EMPLOYERS_LIST[i].series + "^" +
+                                 EMPLOYERS_LIST[i].code + "^";
+                    SW.WriteLine(line);
+                }
+            }
+            SW.Close();
+            fs.Close();
+            label6.Text += "Файл сохранён с именем: " + fileName + "\n"
+                        + "Можно завершить работу программы." + "\n";
+            FileStream fCh = new FileStream(fullPath + "\\RSBank.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            SW = new StreamWriter(fCh);
+            SW.WriteLine(textBox1.Text);
+            SW.Close();
+            fCh.Close();
+        }
+    }
+    public class employer
+    {
+        public string fName;
+        public string lName;
+        public string mName;
+        public string account;
+        public string bik;
+        public string series;
+        public string code;
+
+        public double money;
+
+        public employer(string _lName, String _fName, String _mName, Double _money)
+        {
+            this.fName = _fName;
+            this.lName = _lName;
+            this.mName = _mName;
+            this.money = _money;
+        }
+    }
+}
